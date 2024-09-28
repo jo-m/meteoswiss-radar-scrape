826bd6aea7c5b2d909901ba750b59d847d070199
Jonathan Müller <jo-m@outlook.com>
2024-09-28 16:57:28 +0200

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Automatic scraping of MeteoSwiss radar data
+
+Runs periodically via Github Actions.
+Data is committed to this repo.
+
+Inspired by https://simonwillison.net/2020/Oct/9/git-scraping/.
---
+++ scrape.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+curl "https://www.meteoschweiz.admin.ch/product/output/versions.json" \
+    | jq > versions.json
+anim_version="$(jq -r '.["precipitation/animation"]' < versions.json)"
+
+curl "https://www.meteoschweiz.admin.ch/product/output/precipitation/animation/version__$anim_version/de/animation.json" \
+    | jq > animation.json
+
+jq '.["map_images"][0]["pictures"] | map(select(.data_type == "measurement")) | max_by(.timestamp)' \
+    < animation.json \
+    > last_measurement.json
+
+file_name="$(jq -r '.radar_url | split("/")[-1]' < last_measurement.json)"
+radar_url="https://www.meteoschweiz.admin.ch$(jq -r '.radar_url' < last_measurement.json)"
+
+mkdir -p data/
+echo "Downloading '${radar_url}' to 'data/$file_name'"
+curl "${radar_url}" \
+    > "data/$file_name"
+
+rm *.json
